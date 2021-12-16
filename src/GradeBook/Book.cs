281924551a8c95bc384e4053bf234f3e2873587b
Mike <4xgood@gmail.com>
2021-12-16 09:42:38 -0700

    added high/low grade function to Book class

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,10 +1,35 @@
+using System;
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     class Book
     {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
         public void AddGrade(double grade)
         {
-
+            grades.Add(grade);
         }
+
+        public void ShowStatistics()
+        {
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach(var number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+            }
+            Console.WriteLine($"High grade is {highGrade}");
+            Console.WriteLine($"Low grade is {lowGrade}");
+        }
+
+        List<double> grades;
+        string name;
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,12 +5,13 @@ namespace GradeBook
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-        var book = new Book();
+        var book = new Book("Mike's book");
         book.AddGrade(88.1);
-
+        book.AddGrade(3.4);
+        book.AddGrade(43.41);
+        book.ShowStatistics();
         }
     }
 }
